14888128fa11a5714b62bc63cbc0f3377e80d369
VisHeShYadAVV <visheshyadav06122004@gmail.com>
2025-04-06 20:48:36 +0530

    removing main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from app.api import app
-from fastapi import FastAPI
-from app.recommender import get_recommendations
-
-app = FastAPI()
-
-@app.get("/recommend")
-def recommend(query: str, k: int = 5):
-    return get_recommendations(query, k)
