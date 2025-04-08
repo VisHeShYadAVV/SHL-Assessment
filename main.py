b74112946db0705b6fc3f41396466089a05f9813
VisHeShYadAVV <visheshyadav06122004@gmail.com>
2025-04-09 01:30:21 +0530

    as

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from fastapi import FastAPI
-from app.recommender import get_recommendations
 import uvicorn
+from api import app  
 import os
 
-app = FastAPI()
-
-@app.get("/recommend")
-def recommend(query: str, k: int = 5):
-    return {"results": get_recommendations(query, k)}
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Railway needs this!
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
