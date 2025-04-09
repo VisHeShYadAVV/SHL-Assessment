3011a074402629fdd36d93d4e4f6346b356eb22d
VisHeShYadAVV <visheshyadav06122004@gmail.com>
2025-04-09 10:38:21 +0530

    d

--- app/api.py
+++ app/api.py
@@ -41,5 +41,5 @@ def recommend(query: str, k: int = 5):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
